c16cf1f9800c5fc54210e27f8e0ac5113b18a4d1
diego <dquiozagonzalez@gmail.com>
2024-04-09 20:53:03 -0400

    Imagenes Hero Navbar

---
+++ sources/Customjs/Navbar.js
@@ -0,0 +1,16 @@
+let navBar = document.getElementById("HeroMenu");
+let navBarAnalisis = navBar.getBoundingClientRect();
+let navY = navBarAnalisis.top + window.scrollY
+let navBarChildren= navBar.children;
+window.addEventListener('scroll',()=>{
+    console.log(window.scrollY +" "+ navY);
+    if(window.scrollY >= navY){
+        navBar.style.position = "fixed";
+        navBar.style.backgroundColor = "#F5F0EC";
+        navBar.style.backgroundImage = "none";
+        navBar.style.top="0px";
+        navBar.style.color="#000";
+    }else if(window.scrollY < navY){
+        navBar.removeAttribute('style');
+    }
+});
