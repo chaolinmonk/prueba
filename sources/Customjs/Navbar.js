e959ff288d7d00bb1b6570dbce34f078e09320b5
diego <dquiozagonzalez@gmail.com>
2024-04-14 20:55:24 -0400

    Creacion de Artwork y user

--- sources/Customjs/Navbar.js
+++ sources/Customjs/Navbar.js
@@ -1,15 +1,15 @@
 let navBar = document.getElementById("HeroMenu");
 let navBarAnalisis = navBar.getBoundingClientRect();
-let navY = navBarAnalisis.top + window.scrollY
+let navY = navBarAnalisis.top + window.scrollY;
 let navBarChildren= navBar.children;
 window.addEventListener('scroll',()=>{
-    console.log(window.scrollY +" "+ navY);
+    console.log(window.scrollY +" "+ navY +" "+ navBarAnalisis.top);
     if(window.scrollY >= navY){
         navBar.style.position = "fixed";
         navBar.style.backgroundColor = "#F5F0EC";
         navBar.style.backgroundImage = "none";
         navBar.style.top="0px";
-        navBar.style.color="#000";
+        navBar.style.color="#1F1300";
     }else if(window.scrollY < navY){
         navBar.removeAttribute('style');
     }
